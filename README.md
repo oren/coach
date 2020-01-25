0386fe6a9e16241c8a0caacf330d4abbd443febf
Oren <orengolan@gmail.com>
2020-01-25 15:14:37 -0800

    misc

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# coach
+# Coach
+
+Install
+```
+git clone git@github.com:oren/coach.git
+cd coach
+```
+
+Build
+```
+crystal build src/coach.cr
+```
+
+Run
+```
+./coach
+```
--- src/coach.cr
+++ src/coach.cr
@@ -1,6 +1,28 @@
-# TODO: Write documentation for `Coach`
+require "option_parser"
+
 module Coach
   VERSION = "0.1.0"
 
-	puts "I am your coach!"
+	option_parser = OptionParser.parse do |parser|
+		parser.banner = "I am your coach!"
+
+		parser.on "-v", "--version", "Show version" do
+			puts "version 1.0"
+			exit
+		end
+		parser.on "-h", "--help", "Show help" do
+			puts parser
+			exit
+		end
+	end
+
+	puts "I am your coach."
+	puts ""
+	puts "How can I help you today?"
+	puts "1. Increase my physical strength"
+	puts "2. Learn to dance Cuban Salsa"
+	puts "3. Learn to Surf"
+	puts "4. Learn Chinese"
+	puts ""
+	puts "Choose 1-4 and hit enter"
 end
